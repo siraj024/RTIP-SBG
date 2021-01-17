223c329f3bf6c5946b3f4fc0d8ba28642ea407ed
Muhammad Siraj 024 <salkeen.graphics@gmail.com>
2021-01-17 00:50:29 -0800

    2nd Week Plan Implementation

---
+++ 2nd_Week_Plan_Implementation/app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.currentlocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.audiofx.Equalizer;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    Button btLocation;
+    TextView tvLatitude,tvLongitude;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btLocation = findViewById(R.id.bt_location);
+        tvLatitude = findViewById(R.id.tv_latitude);
+        tvLongitude =findViewById(R.id.tv_longitude);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                {getCurrentLocation();}else {
+                    ActivityCompat.requestPermissions(MainActivity.this ,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                            100);
+                }
+
+                }
+
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults.length > 0 && (grantResults[0]+grantResults[1]
+        == PackageManager.PERMISSION_GRANTED)){
+            getCurrentLocation();
+        }else {
+            Toast.makeText(getApplicationContext(),"Permission Denied",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE
+        );
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                ||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ){
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    final Location location = task.getResult();
+                    if(location != null){
+                        tvLatitude.setText(String.valueOf(location.getLatitude()));
+                        tvLongitude.setText(String.valueOf(location.getLongitude()));
+                    }else {
+                        LocationRequest locationRequest = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(1000)
+                                .setFastestInterval(1000).setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback(){
+                            @Override
+                            public void onLocationResult(LocationResult locationResult){
+                                Location location1 = locationResult.getLastLocation();
+                                tvLatitude.setText(String.valueOf(location1.getLatitude()));
+                                tvLongitude.setText(String.valueOf(location1.getLongitude()));
+                            }
+                        };
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                                locationCallback, Looper.myLooper());
+
+                    }
+
+
+
+                }
+            });
+        }else {
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS
+            ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+}
